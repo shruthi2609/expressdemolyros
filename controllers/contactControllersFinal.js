799864d80b3e624d6bdae32f4a10f4020b67baac
Mona Lisa <sruthi093@gmail.com>
2024-10-07 15:36:28 +0530

    v2.0

--- controllers/contactControllersFinal.js
+++ controllers/contactControllersFinal.js
@@ -10,7 +10,30 @@ const contactDetails=async (req,res)=>{
     }))
  }
 
+ const getUser=async (req,res)=>{
+    const ipCname=req.query.cname
+    const result=await contactsModel.find({cname:ipCname})
+    res.send(JSON.stringify({
+        status:"success",
+        data:result
+    }))
+ }
+
+ const createUser=async (req,res)=>{
+    const data=req.body
+    const newData=new contactsModel(data)
+    const result=await newData.save()
+    console.log(result)
+    if(result._id){
+        res.send({
+            status:"success",
+            data:result
+        })
+    }
+ }
+
+ 
  module.exports={
-    contactDetails
+    contactDetails,getUser,createUser
  }
  
